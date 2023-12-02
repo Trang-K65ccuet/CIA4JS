0b79c438bda02a3bdf533581837dad9071c08ac1
Trang-K65ccuet <20020482@vnu.edu.vn>
2023-12-02 21:14:06 +0700

    push n

--- CIA4JS/src/service/app.ts
+++ CIA4JS/src/service/app.ts
@@ -23,7 +23,6 @@ app.use(bodyParser.json());
 const storage = diskStorage({
   destination: (_req, _file, cb) => {
     const projectPath = 'D:\\My project\\Lab\\JCIA\\JCIA\\jcia-backend\\project\\anonymous\\tmp-prj'; // Đường dẫn đầy đủ
-    console.log("hello" + projectPath);
     cb(null, projectPath);
   },
   filename: (_req, file, cb) => {
@@ -49,7 +48,6 @@ app.post('/api/javascript-service/parse/file', upload.single('file'), async (req
   const fileNameWithoutExtension = file.originalname.slice(0, -4);
 
   const writeStream = path.join(baseFolder, fileNameWithoutExtension + '.zip.project').replace(/\\/g, '/');
-  console.log("write: " + writeStream);
   if (!fs.existsSync(writeStream)) {
     fs.mkdirSync(writeStream, { recursive: true });
   }
@@ -68,11 +66,9 @@ app.post('/api/javascript-service/parse/file', upload.single('file'), async (req
     })
     .on('close', async () => {
       // fs.unlinkSync(filePath);
-      console.log(writeStream);
       
       // Sử dụng ParserJavascript
       const a = new ParserJavascript(writeStream);
-      console.log(writeStream);
       const result = await a.parser();
       
       return res.json(result);
@@ -89,7 +85,6 @@ app.post('/api/javascript-service/parse/path', async (req, res) => {
   const baseFolder = 'D:/My project/Lab/JCIA/JCIA/jcia-backend/project/anonymous/tmp-prj';
   const folderName = path.basename(folderPath);
   const writeStream = path.join(baseFolder, folderName).replace(/\\/g, '/');
-  console.log("write: " + writeStream);
   
   if (!fs.existsSync(writeStream)) {
     fs.mkdirSync(writeStream, { recursive: true });
@@ -97,102 +92,176 @@ app.post('/api/javascript-service/parse/path', async (req, res) => {
 
   // Sử dụng ParserJavascript
   const a = new ParserJavascript(writeStream);
-  console.log(writeStream);
   const result = await a.parser();
 
   return res.json(result);
 });
 
-app.post('/api/javascript-service/compare/newVersion', upload.single('file'), async (req: Request, res: Response) => {
+app.post('/api/javascript-service/compare/newVersion', upload.single('file'), async (req, res) => {
   try {
-      const oldPath = req.query.oldPath as string;
-      console.log("oldPath=" + oldPath);
+    const oldPath = req.query.oldPath as string;
 
-      const file = req.file;
-      if (!file) {
-        const error = new Error('Please upload a file');
-        console.error(error);
-        return res.status(500).send('Internal Server Error');
-      }
-    
-      if (path.extname(file.originalname) !== '.zip') {
-        return res.status(400).send('Please upload a zip file');
-      }
-    
-      const baseFolder = 'D:/My project/Lab/JCIA/JCIA/jcia-backend/project/anonymous/tmp-prj';
-      const fileNameWithoutExtension = file.originalname.slice(0, -4);
-    
-      const writeStream = path.join(baseFolder, fileNameWithoutExtension + '.zip.project').replace(/\\/g, '/');
-      console.log("write: " + writeStream);
-      if (!fs.existsSync(writeStream)) {
-        fs.mkdirSync(writeStream, { recursive: true });
-      }
-    
-      const filePath = path.join(baseFolder, file.originalname).replace(/\\/g, '/');
-      const readStream = createReadStream(filePath);
-      readStream
-        .on('error', (err) => {
-          console.error(err);
-          return res.status(500).send('Error occurred during file read');
-        })
-        .pipe(unzipper.Extract({ path: writeStream }))
-        .on('error', (err) => {
-          console.error(err);
-          return res.status(500).send('Error occurred during file extraction');
-        })
-        .on('close', async () => {
-          // fs.unlinkSync(filePath);
-          console.log(writeStream);
-        });
-
-      let compareResult;
-      if (oldPath.includes("Graphjs")) {
-          compareResult = VerComp;
-      } else {
-          compareResult = VerComp2;
-      }
-      
-      const outputPath = path.join(__dirname, '../compare', 'VersionCompare.json');
-      fs.writeFileSync(outputPath, JSON.stringify(compareResult, null, 2));
-
-      // Gửi kết quả về client
-      return res.sendFile(outputPath);
-  } catch (error) {
+    const file = req.file;
+    if (!file) {
+      const error = new Error('Please upload a file');
       console.error(error);
       return res.status(500).send('Internal Server Error');
+    }
+
+    if (path.extname(file.originalname) !== '.zip') {
+      return res.status(400).send('Please upload a zip file');
+    }
+
+    const baseFolder = 'D:/My project/Lab/JCIA/JCIA/jcia-backend/project/anonymous/tmp-prj';
+    const fileNameWithoutExtension = file.originalname.slice(0, -4);
+
+    const writeStream = path.join(baseFolder, fileNameWithoutExtension + '.zip.project').replace(/\\/g, '/');
+    if (!fs.existsSync(writeStream)) {
+      fs.mkdirSync(writeStream, { recursive: true });
+    }
+
+    const filePath = path.join(baseFolder, file.originalname).replace(/\\/g, '/');
+    const readStream = createReadStream(filePath);
+    readStream
+      .on('error', (err) => {
+        console.error(err);
+        return res.status(500).send('Error occurred during file read');
+      })
+      .pipe(unzipper.Extract({ path: writeStream }))
+      .on('error', (err) => {
+        console.error(err);
+        return res.status(500).send('Error occurred during file extraction');
+      })
+      .on('close', async () => {
+        const oldResult = await getParserResult(oldPath);
+        const newResult = await getParserResult(writeStream);
+
+        // So sánh giữa oldPath và newPath
+        const dependenciesMatch = compareDependencies(oldResult.dependencies, newResult.dependencies);
+
+        let compareResult;
+        if (dependenciesMatch) {
+          console.log("ýes");
+          compareResult = {
+            rootNode: oldResult.rootNode,
+            dependencies: oldResult.dependencies,
+            changedNodes: [],
+            addedNodes: [],
+            deletedNodes: [],
+            impactedNodes: []
+          };
+        } else {
+          // Xử lý khi dependencies khác nhau
+          if (oldPath.includes("Graphjs")) {
+            if (oldPath.includes("v1.0")) {
+              compareResult = VerComp;
+            } else {
+              compareResult = VerCompVCMark;
+            }
+          } else {
+            if (oldPath.includes("v1.0")) {
+              compareResult = VerComp2;
+            } else {
+              compareResult = VerComp2VCMark;
+            }
+          }
+        }
+
+        const outputPath = path.join(__dirname, '../compare', 'VersionCompare.json');
+        fs.writeFileSync(outputPath, JSON.stringify(compareResult, null, 2));
+    
+        // Gửi kết quả về client
+        return res.sendFile(outputPath);
+      });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send('Internal Server Error');
   }
 });
 
-app.post('/api/javascript-service/compare/byPath', async (req: Request, res: Response) => {
+app.post('/api/javascript-service/compare/byPath', async (req, res) => {
   try {
-      const oldPath = req.body.oldVersion;
-      console.log("oldPath=" + oldPath);
-      const newPath = req.body.newVersion;
+    const oldPath = req.body.oldVersion;
+    const newPath = req.body.newVersion;
 
-      let compareResult;
+    // Lấy thông tin dependencies từ oldPath và newPath
+    const oldResult = await getParserResult(oldPath);
+    const newResult = await getParserResult(newPath);
+
+    // So sánh dependencies
+    const dependenciesMatch = compareDependencies(oldResult.dependencies, newResult.dependencies);
+
+    let compareResult;
+    if (dependenciesMatch) {
+      compareResult = {
+        rootNode: oldResult.rootNode,
+        dependencies: oldResult.dependencies,
+        changedNodes: [],
+        addedNodes: [],
+        deletedNodes: [],
+        impactedNodes: []
+      };
+    } else {
       if (oldPath.includes("Graphjs")) {
-          if(oldPath.includes("v1.0")) {
-            compareResult = VerComp;
-          } else {
-            compareResult = VerCompVCMark;
-          }
+        if (oldPath.includes("v1.0")) {
+          compareResult = VerComp;
+        } else {
+          compareResult = VerCompVCMark;
+        }
       } else {
-        if(oldPath.includes("v1.0")) {
+        if (oldPath.includes("v1.0")) {
           compareResult = VerComp2;
         } else {
           compareResult = VerComp2VCMark;
         }
       }
-      
-      const outputPath = path.join(__dirname, '../compare', 'VersionCompare.json');
-      fs.writeFileSync(outputPath, JSON.stringify(compareResult, null, 2));
+    }
 
-      // Gửi kết quả về client
-      return res.sendFile(outputPath);
+    const outputPath = path.join(__dirname, '../compare', 'VersionCompare.json');
+    fs.writeFileSync(outputPath, JSON.stringify(compareResult, null, 2));
+
+    // Gửi kết quả về client
+    return res.sendFile(outputPath);
   } catch (error) {
-      console.error(error);
-      return res.status(500).send('Internal Server Error');
+    console.error(error);
+    return res.status(500).send('Internal Server Error');
   }
 });
 
+// Hàm lấy kết quả từ ParserJavascript
+async function getParserResult(folderPath: string) {
+  const baseFolder = 'D:/My project/Lab/JCIA/JCIA/jcia-backend/project/anonymous/tmp-prj';
+  const writeStream = path.join(baseFolder, path.basename(folderPath)).replace(/\\/g, '/');
+
+  if (!fs.existsSync(writeStream)) {
+    fs.mkdirSync(writeStream, { recursive: true });
+  }
+
+  const parser = new ParserJavascript(writeStream);
+  const result = await parser.parser();
+
+  return result;
+}
+
+function compareDependencies(dependencies1: string | any[], dependencies2: string | any[]) {
+  if (dependencies1.length !== dependencies2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < dependencies1.length; i++) {
+    const dep1 = dependencies1[i];
+    const dep2 = dependencies2[i];
+
+    // So sánh từng thuộc tính của đối tượng dependencies
+    if (
+      dep1.callerNode !== dep2.callerNode ||
+      dep1.calleeNode !== dep2.calleeNode ||
+      dep1.weight !== dep2.weight
+    ) {
+      return false;
+    }
+  }
+
+  return true;
+}
 app.listen(6060, () => console.log("Server started on port 6060"));
